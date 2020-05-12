aa9fe7746bd1991988357f7fa7c42317bedb184c
daniiack <carloscotrim12@gmail.com>
2020-05-12 11:50:29 -0300

    5 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,19 +1,25 @@
 export const products = [
   {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'GeForce GTX 1050 Ti ',
+    price: 837.10,
+    description: 'é uma das placas de vídeo da atualidade mais populares entre o público. O principal diferencial do produto está no custo-benefício. '
   },
   {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'AMD RX 560',
+    price: 1329,
+    description: 'Uma concorrente direta da GTX 1050 é a Radeon RX 560, da AMD, com uma performance semelhante em games e aplicativos em gerais.'
   },
   {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'GeForce GTX 1060 6GB',
+    price: 1294,
+    description: 'brilha com novas tecnologias, tornando-o a escolha perfeita para os jogos de alta definição mais recentes e está pronto para VR. Com o suporte da NVIDIA Pascal - a arquitetura de GPU mais avançada já criada'
+  },
+  {
+    name: 'GeForce GTX 1080 Ti 11GB',
+    price: 4500,
+    description: 'a GeForce GTX 1080 Ti apresenta a tecnologia mais avançada, com 11GB de memória dedicada e um desempenho fora de série.'
   }
+  
 ];
 
 
